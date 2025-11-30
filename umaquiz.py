42e1eaceeca6df7077e15d43c0b9f8d5f01600ab
sim-siraoki <116602710+sim-siraoki@users.noreply.github.com>
2025-11-30 09:47:53 +0900

    Add files via upload

--- umaquiz.py
+++ umaquiz.py
@@ -1,3 +1,19 @@
+from flask import Flask
+import threading
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "I am alive!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = threading.Thread(target=run)
+    t.start()
+
 import discord
 from discord.ext import commands
 import random
